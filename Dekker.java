0acb0be469a04fe2bbe81fde38420cf7909ceb40
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2014-04-15 11:11:42 -0500

    initial commit

---
+++ Dekker.java
@@ -0,0 +1,31 @@
+public class Dekker implements Synchronizer
+{
+   private volatile int favoredprocess;
+   private volatile boolean p0wantstoenter;
+   private volatile boolean p1wantstoenter;
+
+   public Dekker()
+   {
+      favoredprocess = 0;
+      p0wantstoenter = false;
+      p1wantstoenter = false;
+   }
+
+   public void enteringCR(int t)
+   {
+      int other;
+      
+      other = 1 - t;
+  
+      //complete entry protocol
+
+   }
+
+   public void leavingCR(int t)
+   {
+      favoredprocess = 1-t;
+      
+      //complete exit protocol
+
+   }
+}
